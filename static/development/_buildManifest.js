fba43472a9e35023d90c61ce86641533f655470a
pc <tecno3d.info@gmail.com>
2021-10-19 14:55:40 -0500

    Updates

---
+++ static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/carrito":["static\u002Fchunks\u002Fpages\u002Fcarrito.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fcarrito"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
